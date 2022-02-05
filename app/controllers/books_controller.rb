07b38a9e9ce966a86243c85c8591223432ea5067
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-05 03:11:46 +0000

    Add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,14 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  get 'users' => 'users#index'
+  get 'users/:id' => 'users#show'
+  get 'users/:id/edit' => 'users#edit'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
