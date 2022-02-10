67ba71d6d0acb941c383ffc0444d732f3a962185
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-10 11:33:23 +0000

    Finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,21 @@ class User < ApplicationRecord
 
     has_many :books, dependent: :destroy
     has_one_attached :profile_image
-    has_one_attached :image
-         
+
         
-  def get_profile_image(size)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize: size).processed
-  end 
-         
+  # def get_profile_image(size)
+  #   unless profile_image.attached?
+  #     file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+  #     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #   profile_image.variant(resize: size).processed
+  # end 
+  def get_profile_image(width, height)
+  unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  profile_image.variant(resize_to_limit: [width, height]).processed
+  end        
          
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   patch 'books/:id' => 'books#update', as: 'update_book'
   patch 'users/:id' => 'users#update', as: 'update_user'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
