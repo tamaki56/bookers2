cd21f4f9cb47624f87e182a29a8e663976b2a1db
EC2 Default User <ec2-user@ip-172-31-4-156.ec2.internal>
2022-02-10 07:55:02 +0000

    Add

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,9 @@
 class Book < ApplicationRecord
+  belongs_to :user
   
   validates :title, presence: true #空でないように設定
   validates :body, presence: true, length: { maximum: 200 } #空でない、かつ最大200文字までに設定
   
  
-  
-  belongs_to :user
   
 end
--- db/migrate/20220122024305_create_books.rb
+++ db/migrate/20220122024305_create_books.rb
@@ -4,7 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       
       t.integer :user_id
       t.string :title
-      t.string :body
+      t.text :body
       t.timestamps
       
     end
